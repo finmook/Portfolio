c002703d48044d38a27d5ba2a2d578510a786c1e
finmook <apimooksarovath@gmail.com>
2025-07-27 22:19:22 +0700

    fixed navbar and darkMode on other page fixed

--- index.js
+++ index.js
@@ -3,28 +3,45 @@ const darkMode=document.querySelector("body #darkModeSwitch");
 const textToggle=document.querySelector(".textToggle");
 const icon=document.querySelector("#logo");
 const contactBtn=document.querySelector("#contactButton");
+if(localStorage.getItem("darkMode")==true){
+    icon.setAttribute("src","./images/white-book.svg");
+    icon.setAttribute("height","20px")
+    contactBtn.classList.remove("btn-outline-primary");
+    contactBtn.classList.add("btn-outline-light");
+    localStorage.setItem("darkMode", true);
+}
 darkMode.addEventListener("change",()=>{
     if(darkMode.checked){
         icon.setAttribute("src","./images/white-book.svg");
         icon.setAttribute("height","20px")
-        contactBtn.classList.remove("btn-outline-primary");
-        contactBtn.classList.add("btn-outline-light");
+        if(contactBtn){
+            contactBtn.classList.remove("btn-outline-primary");
+            contactBtn.classList.add("btn-outline-light");
+        }
         
+        localStorage.setItem("darkMode", true);
 
     }else{
         icon.setAttribute("src","./images/journals.svg");
-        contactBtn.classList.remove("btn-outline-light");
-        contactBtn.classList.add("btn-outline-primary");
+        if(contactBtn){
+            contactBtn.classList.remove("btn-outline-light");
+            contactBtn.classList.add("btn-outline-primary");
+        }
         
+        localStorage.setItem("darkMode", false);
 
     }
+    
     page.classList.toggle("dark-mode");
     textToggle.classList.toggle("text-toggle");
     
     
     
 })
-contactBtn.addEventListener("click", () => {
-    document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
-  });
+if(contactBtn){
+    contactBtn.addEventListener("click", () => {
+        document.querySelector("#contact").scrollIntoView({ behavior: "smooth" });
+    });
+}
+
 
